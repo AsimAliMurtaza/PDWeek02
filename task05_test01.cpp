aeab028c6b75648004ee22608a9f4473994911a7
Asim Ali Murtaza <mo.asim.murtaza@gmail.com>
2022-12-16 16:10:31 +0500

    These are Programming Day Week02 task projects.

---
+++ task05_test01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+main ()
+{
+cout << ("******************************************") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*          UNIVERSITY ADMISSION          *") << endl;
+cout << ("*            Managment System            *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("*                                        *") << endl;
+cout << ("******************************************") << endl;
+
+
+}
---
+++ task09_cow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main ()
+{
+cout << ("----------------------                  ") << endl;
+cout << ("  o   ^___^                ") << endl;
+cout << ("    o (o o)\\______              ") << endl;
+cout << ("      (___)\\      )\\/\\/            ") << endl;
+cout << ("           ||      |           ") << endl;
+cout << ("           ||---w ||           ") << endl;
+cout << ("           ||     ||           ") << endl;
+cout << ("                          ") << endl;
+
+}
